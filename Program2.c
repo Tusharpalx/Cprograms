023767f665ea5f42e114f0750b77ce3202f1bd8a
Captain levi <67622043+Tusharpalx@users.noreply.github.com>
2021-01-22 23:49:00 +0530

    Add files via upload

---
+++ Program2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+int x,y, sum;
+
+printf("Enter first Number:-");
+scanf("%d",& x);
+
+printf("Enter Second Number:-");
+scanf("%d",& y);
+
+
+sum = x + y;
+
+printf("\nsum: %d", sum);
+
+    
+    
+    getch();
+}
